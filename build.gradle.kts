682179a349765a56b803d5a7698866ffe068b9cd
Daniil Demidko <demidko@farpost.com>
2025-11-24 05:57:18 -0300

    Kotlin & Gradle upd.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ repositories {
   maven("https://jitpack.io")
 }
 plugins {
-  kotlin("jvm") version "1.5.31"
-  id("com.github.johnrengelman.shadow") version "7.1.0"
+  kotlin("jvm") version "2.3.0-RC"
+  id("com.gradleup.shadow") version "9.2.2"
 }
 dependencies {
   implementation("com.github.demidko:aot-bytecode:2021.10.26")
